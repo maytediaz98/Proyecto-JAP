111a85c2d15837d6912ecdf3f4bdceba0b9c6a7e
maytediaz98 <diazmay74@gmail.com>
2021-08-22 00:32:32 -0300

    Entrega 1
    Trabajo de Login y Productos

--- js/login.js
+++ js/login.js
@@ -2,5 +2,28 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+    document.getElementById("entrarBtn").addEventListener("click", function(){
+let inputUsuario = document.getElementById("inputUsuario");
+let inputContraseña = document.getElementById("inputContraseña");
+let datoUsuario = true;
+let datoContraseña = true;
 
+if (inputUsuario.value === ''){
+    datoUsuario = false ; 
+    alert("El campo usuario no puede estar vacío")}
+    else
+    {datoUsuario=true;}
+    
+if (inputContraseña.value === ''){
+    datoContraseña = false;
+    alert(" El campo contraseña no puede estar vacío");
+}
+else
+{datoContraseña = true;}
+
+if (datoUsuario ===true && datoContraseña === true) 
+{
+window.location = "inicio.html";
+}
+});
 });
--- js/product-info.js
+++ js/product-info.js
@@ -1,6 +1,19 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+var product = {};
 
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCT_INFO_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            product = resultObj.data;
+
+            let productNameHTML  = document.getElementById("productName");
+            let productDescriptionHTML = document.getElementById("productDescription");
+            let productPrecioHTML = document.getElementById("productPrecio");
+
+        
+            productNameHTML.innerHTML = product.name;
+            productDescriptionHTML.innerHTML = product.description;
+            productPrecioHTML.innerHTML = product.cost;
+        }
+    });
 });
