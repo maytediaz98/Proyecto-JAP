57bcdef106b435dc3c327a52f1c9d3e7dda2435e
maytediaz98 <diazmay74@gmail.com>
2021-10-17 21:19:42 -0300

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -13,24 +13,24 @@ document.addEventListener("DOMContentLoaded", function(e){
     }); 
 });
 
-function showCarrito(carritosArray){
-let contenido = " ";
-contenido += '<hr>' + 'Nombre:  '  + carritosArray.articles[0].name +'<br>'
+//function showCarrito(carritosArray){
+//let contenido = " ";
+//contenido += '<hr>' + 'Nombre:  '  + carritosArray.articles[0].name +'<br>'
 
-contenido += 'Costo por unidad:  '  + carritosArray.articles[0].unitCost +'<br>'
-contenido += 'En moneda:  ' + carritosArray.articles[0].currency +'<br>'
-contenido += '<br> Subtotal:  ' + carritosArray.articles[0].unitCost * 2 +'<br>'
+//contenido += 'Costo por unidad:  '  + carritosArray.articles[0].unitCost +'<br>'
+//contenido += 'En moneda:  ' + carritosArray.articles[0].currency +'<br>'
+//contenido += '<br> Subtotal:  ' + carritosArray.articles[0].unitCost * 2 +'<br>'
 //contenido += 'Vista del producto seleccionado: '+ '<br>' + `<img src="`  + carritosArray.articles[0].src + `" class="img-thumbnail">` + '<hr>'
 //contenido +=  'Cantidad seleccionada:  '  + carritosArray.articles[0].count +'<br>'
 
-let total = carritosArray.articles[0].unitCost * carritosArray.articles[0].count;
-total = total + total * 0.15;
-document.getElementById("total").innerHTML = total;
+//let total = carritosArray.articles[0].unitCost * carritosArray.articles[0].count;
+//total = total + total * 0.15;
+//document.getElementById("total").innerHTML = total;
 
-document.getElementById("carritoArticulo").innerHTML = contenido;
+//document.getElementById("carritoArticulo").innerHTML = contenido;
 
 };
-
+// finaliza Pauta 1
 
 document.addEventListener("DOMContentLoaded", function(e){
     document.getElementById("modificarBtn").addEventListener("click", function(){
@@ -39,9 +39,6 @@ document.addEventListener("DOMContentLoaded", function(e){
 contenido += '<hr>' + 'Nombre:  '  + carritosArray.articles[0].name +'<br>'
 contenido += 'Costo por unidad:  '  + carritosArray.articles[0].unitCost +'<br>'
 contenido += 'En moneda:  ' + carritosArray.articles[0].currency +'<br>'
-
-//contenido += 'Vista del producto seleccionado: '+ '<br>' + `<img src="`  + carritosArray.articles[0].src + `" class="img-thumbnail">` + '<hr>'
-//contenido +=  'Cantidad seleccionada:  '  + carritosArray.articles[0].count +'<br>'
 let subtotal = carritosArray.articles[0].unitCost * inputUnidades;
 let total = carritosArray.articles[0].unitCost * inputUnidades;
 total = total + total * 0.15;
